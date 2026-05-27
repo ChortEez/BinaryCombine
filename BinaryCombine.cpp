66753fb2015363640b837b52211346f222ec7a4b
ChortoPhay <kazmirukmark@gmail.com>
2026-05-27 20:26:07 +0300

    Write some binary sum logic. Need completely rewrite

--- BinaryCombine.cpp
+++ BinaryCombine.cpp
@@ -2,30 +2,78 @@
 #include <cstdint> 
 #include <string>
 #include <math.h>
+#include <iomanip>
+
+
+
+
+
+
+// std::cout << ((number >> i) & 1);
+//         if((i % 4) == 0) std::cout <<" ";
+
+
 
 //Розрядність системи - 16 біт. Всюди буду юзати uint16_t
-void decimalToBinary(uint16_t number){
+// Для двійкової системи - стрінг.
+std::string decimalToBinary(uint16_t number){
+    std::string result;
     for (int i = 15; i >= 0; i--){
-        std::cout << ((number >> i) & 1);
-        if((i % 4) == 0) std::cout <<" ";
+        result.push_back('0' + ((number >> i) & 1));
+        if((i % 4) == 0) result.push_back(' ');
     }
-    std::cout << '\n';
+    return result;
 }
-void binaryToDecimal(std::string number){ //1010 3
+uint16_t binaryToDecimal(std::string number){ //1010 3
     uint16_t result;
-    int j = -1;    //Віднімаємо 1 для синхронізації i з j.
-    for(int i = number.length(); i >= 0; i--){
+    int j = 0;    //Віднімаємо 1 для синхронізації i з j.
+    for(int i = number.length() - 1; i >= 0; i--){
         if(number[i] == '1') {
             result += pow(2, j);
         }else if(number[i] == ' ') continue;
+        
+        if(number[i] != '1' && number[i] != '0'){
+            std::cout <<"НЕВАЛІДНИЙ ВВІД!"; 
+            break;
+        }
         j++;
     }
-    std::cout << result;
+    return result;
     std::cout << '\n';
+}
+void binaryAdd(std::string num1, std::string num2){ //1010 0101 //ПЕРЕПИШИ ЛОГІКУ ПЕРЕВІРКИ ПЕРЕПОВНЕННЯ
+    const uint16_t maximal = 65535;
+    uint16_t number1, number2, temp;
+    uint16_t result;
+    number1 = binaryToDecimal(num1);
+    number2 = binaryToDecimal(num2);
+    
+    if (number1 > number2){
+        temp = number1 - number2;
+        if(temp > number2){
+            std::cout << "ПЕРЕПОВНЕННЯ. ЧИСЛА ЗАВЕЛИКІ";
+            return;
+        }
+    } else if(number2 > number1){
+        temp = number2 - number1;
+        if (temp > number1){
+            std::cout << "ПЕРЕПОВНЕННЯ. ЧИСЛА ЗАВЕЛИКІ";
+            return;
+        }
+    }
+
+
+    result = number1 + number2;
+
+    std::cout << std::setw(32) << num1 << '\n';
+    std::cout << std::setw(32) << num2 << '\n';
+    std::cout << std::setfill('-')<< std::setw(33) << '\n'; // Розділювач. З невідомих причин звідкись береться ще один символ, і при значенні зсуву в 32, рівняється на один символ лівіше.
+    std::cout << std::setfill(' ') << std::setw(33) << decimalToBinary(number1 + number2);
+    
 }
 
 
 int main(){
-    decimalToBinary(11);
-    binaryToDecimal(" 1000 0000 0000 0000");
+    binaryAdd("0111 1111 1111 1111","0111 1111 1111 1111");
+    // std::cout << decimalToBinary(32767);
 }
